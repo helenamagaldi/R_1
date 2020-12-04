33eba1f4281471fcaf832a31fac26273df39165a
Helena Magaldi <helenamagaldi@gmail.com>
2020-12-04 17:33:50 -0300

    x

--- 2. Machine Learning/Machine_Learning.R
+++ 2. Machine Learning/Machine_Learning.R
@@ -11,3 +11,36 @@ dataset <- read.csv(filename, header=FALSE)
 
 #setting columns names
 colnames(dataset) <- c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width","Species")
+
+# splitting loaded dataset 80% (working Dataset) - 20% (validation dataset). I'm now reducing my working dataset into 80% of its original content.
+
+validation_index <- createDataPartition(dataset$Species, p=0.80, list=FALSE)
+
+validation <- dataset[-validation_index,]
+
+# use the 80% to test the models
+dataset <- dataset[validation_index,]
+
+# to view:
+# > View(validation_index)
+# > View(dataset)
+
+# dataset dimensions: instances(rows) x attributes(columns)
+dim(dataset)
+# [1] 120   5
+
+# types of attributes: list for each one of the attributes
+sapply(dataset, class)
+#Sepal.Length  Sepal.Width Petal.Length  Petal.Width      Species 
+# "numeric"    "numeric"    "numeric"    "numeric"  "character" 
+
+# first 5 rows
+head(dataset)
+
+# levels for the class
+levels(dataset$Species)
+
+# summarizing class distribution: each class has the same number of instances (40 occurances or 33%)
+percentage <- prop.table(table(dataset$Species)) * 100
+cbind(frec=table(dataset$Species), percentage=percentage)
+
--- 2. Machine Learning/txt.md
+++ 2. Machine Learning/txt.md
@@ -1,1 +1,4 @@
 https://machinelearningmastery.com/machine-learning-checklist/
+
+
+https://machinelearningmastery.com/machine-learning-in-r-step-by-step/
